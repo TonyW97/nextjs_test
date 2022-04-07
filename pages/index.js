7b2779c397a79e1f7acf25eb51e20ae419bc0cd8
Tony Wang <tw344466@gmail.com>
2022-04-06 21:58:09 -0400

    First commit

---
+++ pages/index.js
@@ -0,0 +1,43 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../components/layout'
+import Image from 'next/image'
+import { getSortedPostsData } from '../lib/posts'
+import utilStyles from '../styles/utils.module.css'
+import Date from '../components/dates'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+    <Head>
+      <title>A app</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+            <Link href={`/posts/${id}`}>
+              <a>{title}</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
